1eaecac8555f985eeb7f36ba027ad111be7fc7a0
Emircandgn <94379692+Emircandgn@users.noreply.github.com>
2021-11-28 13:21:24 +0300

    Add files via upload

---
+++ hommefinito.py
@@ -0,0 +1,54 @@
+
+
+
+paswword = input("Merhaba Eve Girmek İçin Lütfen Şifreyi Giriniz:")
+if paswword == "0314":
+                    print("     +")
+                    print("   +    +")
+                    print("  +      +")
+                    print(" +--------+")
+                    print("!    .-.   !")
+                    print("!    ! !   !")
+                    print("+-+-+-+-+-+-")
+                    print("Hoşgeldin bendeniz CARLOS emrine amadeyim")
+                    how=input("Günün Nasıl Geçti?: ")
+                    if how == "Kötüydü":
+                        print("Hadi ya, o zaman seni biraz neşelendirelim.")
+                        print("Güzel bir duş alıp kendine gelmek ister misin ya da oyun oynamak?")
+                        oyun=input("")
+                        if oyun == "olur":
+                            print("Mükkemmell bilgisayarını açıyorum.")
+                            print("Hangi oyunu oynamak istersin? ")
+                            which=input("")
+                            if which=="lol":
+                                    print("Kanser olma zamanı desene :d")
+                            if which=="cs":                               
+                                    print("Tamam ama bıçak bıçağa silah çekersen bozuşuruz:d")
+                            if which=="witcher":
+                                    print("Bende Ciriyi çok özlemiştim oley")
+                            else:
+                                    print("Başlayalımm Başlayalımm!")
+                        else:
+                            print("peki çok güzel bi yemek denedim bil bakalım ne?")
+                            yemek=input("")
+                            if yemek=="Pide":
+                                print("Kahin mi sikti seni sürprizim bozuldu amaa")
+                            else:
+                                print("bilemedin buna bayılacaksınn")
+                                print("     *")
+                                print("  *  *  *")
+                                print(" * *   * *")
+                                print("*---------*")
+                                print("!    .-.   !")
+                                print("!  *    *  !")
+                                print("+-+-######-+-")
+                                print("!  *    *  !")
+                                print("!    .-.   !")
+                                print("* ---------*")
+                                print(" *  *   * *")
+                                print("  *  *  *")
+                                print("     * ")
+                                print("TADAAA kuşbaşılı pideeee bon appetitt gülüm")
+
+                    else:print("Ah ne güzel,senin için film seçiyorum.")
+else :  print("Girdiğiniz şifre yanlış lütfen tekrar deneyiniz.")
